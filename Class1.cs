e8f257ca00dfecd120b220b5549c0bdc5e93d83f
Munkhjargal1995 <83129536+Munkhjargal1995@users.noreply.github.com>
2021-12-14 20:58:57 +0800

    Class Lesson

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp9
+{
+    class Class1 // Class1 = newBook()
+    {
+        public string BookName = "The Fault in our stars";
+        public string Author = "John Green";
+        public string PublishDate = "11/20/2013";
+        public int BookSold = 2000;
+        public string Genre = "Young adult fiction";
+
+        public string BookName2 = "Paper towns";
+        public string Author2 = "John Green";
+        public string PublishDate2 = "10/5/2005";
+        public int BookSold2 = 1500;
+        public string Genre2 = "Young adult fiction";
+
+    }
+    class Book2
+    {
+        public string BookName = "Midnight Sun";
+        public string Author = "Stephenie Meyer";
+        public string PublishDate = "7/2/2020";
+        public int BookSold = 1230;
+        public string Genre = "Vampire fiction,Young adult, love romance";
+
+        public string BookName2 = "Twilight";
+        public string Author2 = "Stephenie Meyer";
+        public string PublishDate2 = "10/5/2005";
+        public int BookSold2 = 700;
+        public string Genre2 = "Vampire fiction,Young adult,Horror";
+    }
+
+}
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 newBook = new Class1();
+            Book2 Midnight = new Book2();
+
+            Midnight.Author = textBox1.Text;
+            newBook.Author = textBox1.Text;
+
+            string fault = newBook.Author = "John Green";
+            string night = Midnight.Author = "Stephenie Meyer";
+
+            if(textBox1.Text == "Stephenie Meyer" || textBox1.Text == "Meyer"
+                || textBox1.Text == "Stephen" || textBox1.Text =="stephenie meyer")
+            {
+                string twilight = string.Format("Author:{0},BookName:{1},PublishDate:{2},BookSold:{3},Genre:{4}", night, Midnight.BookName, Midnight.PublishDate, Midnight.BookSold, Midnight.Genre); //ehnii nom Meyeriin
+                string twilight2 = string.Format("Author:{0},BookName:{1},PublishDate:{2},BookSold:{3},Genre:{4}", night, Midnight.BookName2, Midnight.PublishDate2, Midnight.BookSold2, Midnight.Genre2); // daraagiin nom
+                listBox1.Items.Add(twilight);
+                listBox1.Items.Add(twilight2);
+            }
+            else if(textBox1.Text == "John Green" || textBox1.Text =="John" ||
+                textBox1.Text == "john" || textBox1.Text == "Green")
+            {
+                string nom1 = string.Format("Author:{0},BookName:{1},PublishDate:{2},BookSold:{3},Genre:{4}", fault, newBook.BookName, newBook.PublishDate, newBook.BookSold, newBook.Genre); // ehnii nom John Green
+                string nom2 = string.Format("Author:{0},BookName:{1},PublishDate:{2},BookSold:{3},Genre:{4}", fault, newBook.BookName2, newBook.PublishDate2, newBook.BookSold2, newBook.Genre2); // daraagiin nom
+                listBox1.Items.Add(nom1);
+                listBox1.Items.Add(nom2);
+            }
+            else // textBox1.Text != "Stephenie Meyer"
+            {
+                MessageBox.Show("iim zohiolch bhgui bn", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
